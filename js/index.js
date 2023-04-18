5b92bb73c3c1e6c95b12bb0a63e720ac48484614
凯罗 <123644070+kailuo18822263362@users.noreply.github.com>
2023-04-18 07:37:50 -0700

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,16 @@
+
+function  mymethon(){
+    var JSONObject= {
+        "name":"菜鸟教程",
+        "url":"www.runoob.com", 
+        "slogan":"学的不仅是技术，更是梦想！"
+    };
+
+    document.getElementById("jname").innerHTML=JSONObject.name ;
+    document.getElementById("jurl").innerHTML=JSONObject.url ;
+    document.getElementById("jslogan").innerHTML=JSONObject.slogan ;  
+    
+}
+
+
+
